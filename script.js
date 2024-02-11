f60b1dc094291e633ea9caaa2925c19459b1051b
Shreyash Tailor <151995519+Captain1678@users.noreply.github.com>
2024-02-11 17:24:27 +0530

    Added new game : Snake, Water, Gun.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // :D
 
+
+/* Code func */
 const myfunc = () => {
     var ab;
     do {
@@ -12,13 +14,10 @@ const myfunc = () => {
     window.alert("Correct number entered!");
 };
 
-    
+  /* Changing background according to user */  
         const secondfunc = () => {
             let abcd = window.prompt("Enter the color you want to apply to this page ^_^.\nThe colors are: Yellow, Red, and Purple and if you want to revert it back, type default",); // 2nd "" is default value
 
-            if(abcd === null){
-                return;
-            }
             if (abcd === "Yellow") {
                 document.body.style.backgroundColor = "yellow";
             } else if (abcd === "Red") {
@@ -35,3 +34,34 @@ const myfunc = () => {
             }
         };
         
+
+/*Snake, Water and Gun Game */
+
+const swg = ()=>{
+let again=true;
+while(again){
+let random = Math.floor(Math.random() * 3) + 1;
+switch (random) {
+  case 1:
+    random = 'snake';
+    break;
+  case 2:
+    random = 'water';
+    break;
+  case 3:
+    random = 'gun';
+    break;
+}
+let user= window.prompt("Enter either Snake or Water or Gun");
+if(random===user){
+  alert(`Its a Draw\n Computer: ${random}\n User: ${user}`);
+}
+else if((random==='s' && user==='w') || (random==='w' && user==='g') || (random==='g' && user==='s')){
+  alert(`Sorry! You Lost\n Computer: ${random}\n User: ${user}`);
+}
+else if((random==='s' && user==='g') || (random==='w' && user==='s') || (random==='g' && user==='w')){
+  alert(`Congratulations! You Won\n Computer: ${random}\n User: ${user}`);
+}
+  again= confirm('Do u want to play again');
+}
+}
